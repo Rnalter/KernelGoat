6a863af641d5ee4ac2006e26a1db1f6719766d81
sujithapv10 <159543575+sujithapv10@users.noreply.github.com>
2024-09-17 02:43:04 -0400

    Delete .Module.symvers.cmd

--- .Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/sujitha/Desktop/sampleprog/Module.symvers := sed 's/\.ko$$/\.o/' /home/sujitha/Desktop/sampleprog/modules.order | scripts/mod/modpost -m -a  -o /home/sujitha/Desktop/sampleprog/Module.symvers -e -i Module.symvers   -T -
