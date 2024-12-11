82385bb528f4d8bc2e25878711fd8a6ddcbbeca8
mrxlimitless <>
2024-12-11 21:22:09 +0530

    removed uneccesary folder

--- guide/build/assets/js/538b959a.3ba72b49.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkguide=self.webpackChunkguide||[]).push([[2529],{9073:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/KernelGoat/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
