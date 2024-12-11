6ff0b6cbd2fc386cf027fe9bbe0c4355358e3c9a
mrxlimitless <>
2024-12-11 12:30:15 +0530

    minor changes

--- guide/src/components/HomepageFeatures/index.tsx
+++ guide/src/components/HomepageFeatures/index.tsx
@@ -10,32 +10,29 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'Easy to Setup',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Simple installation steps
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Practice Vulnerabilities',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Understand how kernel Vulnerabilities are present in the kernel
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Let\'s Hack',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Use your hacking skills to crash the kernel
       </>
     ),
   },
